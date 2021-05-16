fa98e61613caf5363ede0ccd5797b0e34165f01b
Ashish Ranjan <ashish.ranjan@payu.in>
2021-05-16 17:34:42 +0530

    added next app

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
+// Import Koa / Dotenv / Fetch modules
 require("isomorphic-fetch");
 const Koa = require("koa");
-const Router = require('koa-router');
+const KoaRouter = require('koa-router');
 const next = require('next');
+const static = require("koa-static");
+const mount = require("koa-mount");
+var bodyParser = require('koa-bodyparser');
+const session = require("koa-session");
+const cors = require('@koa/cors');
 const dotenv = require("dotenv");
-
-// const { gqlRoutes } = require('./server/graphql/routes');
-const { restApiRoutes } = require('./server/rest/routes');
 
 // Import Shopify/Koa modules to assist with authentication
 const { verifyRequest } = require("@shopify/koa-shopify-auth");
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
 
-// Env Configuration and Context
+// Env Configuration
 dotenv.config();
+const { SHOPIFY_API_SECRET, SHOPIFY_API_KEY } = process.env;
 const port = process.env.PORT || 3000;
+
 Shopify.Context.initialize({
-  API_KEY: process.env.SHOPIFY_API_KEY,
-  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: ["read_products", "write_products", "read_orders", "read_draft_orders"],
-  HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
-  API_VERSION: ApiVersion.October20,
-  IS_EMBEDDED_APP: true,
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
-});
+    API_KEY: process.env.SHOPIFY_API_KEY,
+    API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+    SCOPES: ["read_products", "write_products", "read_orders"],
+    HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
+    API_VERSION: ApiVersion.October20,
+    IS_EMBEDDED_APP: true,
+    SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+  });
 
 // Next app
 const dev = process.env.NODE_ENV !== 'production';
@@ -32,59 +37,63 @@ const handle = app.getRequestHandler();
 
 const ACTIVE_SHOPIFY_SHOPS = {};
 
-// Koa Server
-const server = new Koa();
+// Koa server
+// server.use(session(server));
+// server.keys = [SHOPIFY_API_SECRET];
 
-// Router 
-const router = new Router();
+// Import and use server-side routes
+// const { router } = require('./server/graphql/routes');
+// const { RestApiRoutes } = require('./server/rest/routes');
+// server.use(RestApiRoutes.routes());
+// server.use(RestApiRoutes.allowedMethods());
 
-// Test Route
-router.get('/test', ctx => {
-  ctx.body = `Hello, World`
-});
-
-// GraphQL / REST Routes
-// server.use(gqlRoutes.routes()).use(gqlRoutes.allowedMethods());
-server.use(restApiRoutes.routes()).use(restApiRoutes.allowedMethods());
-server.use(router.routes()).use(router.allowedMethods());
-
-// 
 app.prepare().then(() => {
-  server.keys = [Shopify.Context.API_SECRET_KEY];
+    const server = new Koa();
+    const router = new KoaRouter();
 
-  server.use(
-    createShopifyAuth({
-      afterAuth(ctx) {
-        const { shop, scope } = ctx.state.shopify;
-        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+    server.keys = [Shopify.Context.API_SECRET_KEY];
 
-        ctx.redirect(`/?shop=${shop}`);
-      },
-    }),
-  );
+    server.use(
+        createShopifyAuth({
+          afterAuth(ctx) {
+            const {shop, scope} = ctx.state.shopify;
+            ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+    
+            ctx.redirect(`/?shop=${shop}`);
+          },
+        }),
+      );
 
-  const handleRequest = async (ctx) => {
-    console.log(`Inside handleRequest`);
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-  };
+    const handleRequest = async (ctx) => {
+        console.log(`Inside handleRequest`);
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+        ctx.res.statusCode = 200;
+    };
 
-  router.get("/", async (ctx) => {
-    const shop = ctx.query.shop;
-    if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
-      ctx.redirect(`/auth?shop=${shop}`);
-    } else {
-      await handleRequest(ctx);
-    }
-  });
+    router.get("/", async (ctx) => {
+        const shop = ctx.query.shop;
+    
+        if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
+          ctx.redirect(`/auth?shop=${shop}`);
+        } else {
+          await handleRequest(ctx);
+        }
+      });
 
-  // Next static files
-  router.get("(/_next/static/.*)", handleRequest);
-  router.get("/_next/webpack-hmr", handleRequest);
-  router.get("(.*)", verifyRequest(), handleRequest);
+    // Routes
+    router.get('/test', ctx => {
+        ctx.body = `Hello, World`
+    });
 
-  server.listen(port, () => {
-    console.log(`server listening to port --> ${port}`)
-  });
+    router.get("(/_next/static/.*)", handleRequest);
+    router.get("/_next/webpack-hmr", handleRequest);
+    router.get("(.*)", verifyRequest(), handleRequest);
+
+    server.use(router.allowedMethods());
+    server.use(router.routes());
+
+    server.listen(port, () => {
+        console.log(`server listening to port --> ${port}`)
+    });
 });
