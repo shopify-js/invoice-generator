56a4c3f6c0c63cc2a3483c6ed8260a0fee9a4106
Ashish Ranjan <ashish.ranjan@payu.in>
2021-05-23 20:47:50 +0530

    added mongodb connection

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -35,7 +35,7 @@ async function dbConnect() {
     }
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      console.log(`connected to mongoDB`);
+      console.log(`===== connected to mongoDB =====`);
       return mongoose
     })
   }
